70a5678b25ea3c3dc0549c8c5bd3b743edc513df
Andre Sch <andresch.dev@gmail.com>
2023-09-09 11:44:20 -0300

    Remove error throw from CreateProductService

--- src/features/CreateProduct/CreateProductService.ts
+++ src/features/CreateProduct/CreateProductService.ts
@@ -1,6 +1,5 @@
 import { Product } from "../../entities/Product";
 import { IProductsRepository } from "../../repositories/IProductsRepository";
-import { OperationalError } from "../../errors/OperationalError";
 
 export interface IProductCreationRequest {
   name: string;
@@ -14,10 +13,8 @@ export class CreateProductService {
 
   public async execute(request: IProductCreationRequest): Promise<void> {
     const { name, description, quantity, price } = request;
-    const product = new Product(name, description, quantity, price);
 
-    if (await this.productsRepository.findById(product.id))
-      throw new OperationalError("Product ID is already taken!");
+    const product = new Product(name, description, quantity, price);
 
     await this.productsRepository.create(product);
   }
